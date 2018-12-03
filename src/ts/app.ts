a7d733f49ea875907f055bb128ac8ff19838365a
al <alexander.loktev.stratis@gmail.com>
2018-12-03 18:14:43 +0200

    Added slider in project, set up ts config.

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,5 +1,15 @@
 import "@babel/polyfill";
 import Dropdown from '../../core/components/dropdown/dropdown';
+import * as Swiper from 'swiper/dist/js/swiper';
 
 let dropdown = new Dropdown();
-console.log(dropdown.getSelector() );
+// console.log(dropdown.getSelector() );
+
+
+let swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
